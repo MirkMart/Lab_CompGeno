6d1fdddf094d18f12428b8de99749c34c687b9d9
MariangelaIannello <79593933+MariangelaIannello@users.noreply.github.com>
2022-06-06 10:24:05 +0200

    Update DE.R

--- 8_Differential_Expression/DE.R
+++ 8_Differential_Expression/DE.R
@@ -8,11 +8,11 @@ library(NOISeq)
 matrix_dg <- read.table("pernoiseq.txt", sep = " ",header = TRUE, row.names = 1)
 View(matrix_dg)
 factors <- data.frame(dg_conditions = c("Aste","Aste","Aste","Hill","Hill","Hill"))
-
 dg_noiseq <- readData(data=matrix_dg, factors = factors)
-dg_noiseq
-head(assayData(dg_noiseq)$exprs)
-#dg_noiseq <- addData(dg_noiseq, length = mylength)
+mycountsbio = dat(dg_noiseq, factor = NULL, type = "countsbio")
+explo.plot(mycountsbio, toplot = 1, samples = NULL, plottype = "barplot")
+myfilt2 = filtered.data(matrix_dg, factor = factors$dg_conditions, norm = FALSE, depth = NULL, method = 1, cv.cutoff = 100, cpm = 2, p.adj = "fdr")
+
 
 # look at data quality
 
@@ -21,14 +21,28 @@ explo.plot(mysaturation, toplot = 1, samples = 1:6, yleftlim = NULL, yrightlim =
 mycountsbio = dat(dg_noiseq, factor = NULL, type = "countsbio")
 explo.plot(mycountsbio, toplot = 1, samples = NULL, plottype = "barplot")
 
+#retrieve list of filtered genes
+myfilt10 = filtered.data(matrix_dg, factor = factors$dg_conditions, norm = FALSE, depth = NULL, method = 1, cv.cutoff = 100, cpm = 10, p.adj = "fdr")
+dg_noiseq <- readData(data=myfilt10, factors = factors)
+
+
+filtered_genes=read.table("Aste_Hill_rc_len.txt", header=T)
+# calculate reads per Kbp of gene length (corrected for gene length)
+# gene length is in bp in exppression dataset and converted to Kbp
+rpk_Aste <- (filtered_genes[,2:4]*10^3 )/filtered_genes[,1]
+rpk_Hill<-(filtered_genes[,6:8]*10^3 )/filtered_genes[,5]
+rpk<-cbind(rpk_Aste,rpk_Hill)
+
+dg_noiseq <- readData(data=matrix_dg, factors = factors)
+dg_noiseq
+head(assayData(dg_noiseq)$exprs)
+
 #filter loci with low counts
 
 myfilt10 = filtered.data(matrix_dg, factor = factors$dg_conditions, norm = FALSE, depth = NULL, method = 1, cv.cutoff = 100, cpm = 10, p.adj = "fdr")
 dg_noiseq <- readData(data=myfilt10, factors = factors)
-mycountsbio = dat(dg_noiseq, factor = NULL, type = "countsbio")
-explo.plot(mycountsbio, toplot = 1, samples = NULL, plottype = "barplot")
 dg_TMM10 = tmm(assayData(dg_noiseq)$exprs, long = 1000, lc = 0)
-#lc = 0 and long = 1000 -->no length correction is applied; if we use FPKM --> lc = 1
+#lc = 0 and long = 1000 -->no length correction is applied
 write.table(dg_TMM10, file="dg_tmm10", append = FALSE, eol="\n", quote = FALSE)
 
 
