f57158565aafb6e2febbf232a41b1492a305c12d
Mirko Martini <mirko.martini.science@gmail.com>
2024-08-27 23:38:29 +0200

    Delete 2022 directory

--- 2022/0_Bash/02_files/.svn/pristine/a3/a3cb6af89d367521f5477bea75d17d37db1d281f.svn-base
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#$1=list to grep in $2 
-#$2=where to search
-
-
-file="$1" ; name=$(cat $file) ; for locus in $name ; do grep -w $locus "$2" ; done > "$1"_out
--- 2022/0_Bash/02_files/scripts/oneline_fasta.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#$1=fasta to unwrapp
-
-awk '!/^>/ { printf "%s", $0; n = "\n" } /^>/ { print n $0; n = "" } END { printf "%s", n }' $1 > $1".tmp" && mv $1".tmp" $1
